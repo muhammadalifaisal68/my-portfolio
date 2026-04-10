de7471ffc6a347cbd3be84d88eb3797614987191
Muhammad Ali Faisal <alifaisal68@icloud.com>
2026-04-10 16:53:44 +0500

    Add admin route

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import StudioPage from './studio.jsx'
@@ -11,6 +11,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/studio/*" element={<StudioPage />} />
+        <Route path="/admin/*" element={<StudioPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
